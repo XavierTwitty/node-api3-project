878038f736cf676b0f96efc35fcadeb448a8413b
XavierTwitty <x.twitty@gmail.com>
2022-03-09 14:22:03 -0500

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const User = require("./users-model")
+const Post = require("../posts/posts-model");
 // The middleware functions also need to be required
 
 const router = express.Router();
